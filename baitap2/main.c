0748fa2dffc94b655a4cda77f43682e37848110f
Anhdao1806 <daoleho123@gmail.com>
2024-12-17 12:01:40 +0700

    Add files via upload

---
+++ baitap2/main.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    a=10;
+    printf("Nhap so b ");
+    scanf("%d",&b);
+    while (b!=a ){
+        printf ("Nhap lai b :");
+        scanf ("%d",&b);
+        
+    }
+    printf (" So b da bang so a \n");
+    
+    return 0;
+}
+
